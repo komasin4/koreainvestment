2b727cd3c279548978ddcab55d685760cad29d2b
KOMA-MIIX520\komas <komasin4@gmail.com>
2023-10-26 14:04:12 +0900

    CallUtil 분리

--- src/main/java/com/komasin4/finance/kis/controller/AuthController.java
+++ src/main/java/com/komasin4/finance/kis/controller/AuthController.java
@@ -1,11 +1,15 @@
 package com.komasin4.finance.kis.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import com.komasin4.finance.kis.util.CallUtil;
 
 import org.json.JSONObject;
 
@@ -25,48 +29,37 @@ public class AuthController {
 
 	@Value("${secret.appsecret}")
 	private String secretKey;
-
-	@GetMapping("/api/auth")
-	public @ResponseBody String auth() {
-		log.debug("appKey:" + appKey);
-		log.debug("secretKey:" + secretKey);
-		log.debug("url:" + url);
-
-
-		//    	WebClient webClient = WebClient
-		//                .builder()
-		//                .baseUrl("http://localhost:8080")
-		//                .defaultCookie("쿠키","쿠키값")
-		//                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-		//                .build();
+	
+	@Autowired
+	private CallUtil callUtil;
+	
+	@GetMapping("/api/getCurrent")
+	public @ResponseBody Object getCurrentValue(
+					 @RequestParam(value="FID_COND_MRKT_DIV_CODE") String code
+					,@RequestParam(value="FID_INPUT_ISCD") String isCd
+	) {
+    
+//		FID_COND_MRKT_DIV_CODE	FID 조건 시장 분류 코드	String	Y	2	J : 주식, ETF, ETN
+//		FID_INPUT_ISCD
 		
+		JSONObject param = new JSONObject();
+
+		param.put("FID_COND_MRKT_DIV_CODE", code);
+		param.put("FID_INPUT_ISCD", isCd);
+
+		JSONObject oRtn = callUtil.call("/oauth2/tokenP", null, param);
+		return oRtn;
+	}
+	
+	@GetMapping("/api/auth")
+	public @ResponseBody Object getToken() {
 		JSONObject param = new JSONObject();
 		param.put("grant_type", "client_credentials");
 		param.put("appkey", appKey);
-		param.put("secretkey", secretKey);
+		param.put("appsecret", secretKey);
+
+		JSONObject oRtn = callUtil.call("/oauth2/tokenP", null, param);
 		
-		log.debug("param:" + param.toString());
-
-
-		WebClient webClient = WebClient
-				.builder()
-				.baseUrl(url)
-				.defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json; utf-8")
-				.build();
-
-		String result = webClient.post()
-				.uri("/oauth2/Approval")
-				.bodyValue(param.toString())
-				.retrieve()
-				.bodyToMono(String.class)
-				.onErrorResume(e -> Mono.empty())
-				.block();
-
-		log.debug("result:" + result);
-
-		return "auth";
+		return oRtn;
 	}
-
-
-
 }
